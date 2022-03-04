0c31d2b8044ad34cf45d15c39c777650546edc96
Queen-02 <queenzyblack176@gmail.com>
2022-03-04 08:51:16 +0300

    add changes made to main file

--- src/main.kt
+++ src/main.kt
@@ -1,29 +1,43 @@
 fun main() {
-    val name = "Akirachix"
+    //prints out the first, third and fourth
+    characters("Akirachix")
+
+    //returning the greetings and age
+    var greet = credits("Quienzy", 23)
+    println(greet)
+
+    //returning the length of a word
+    var length = words("mesopotamia")
+    println(length)
+
+    //if else functions
+    crew("Queenzy")
+}
+//function that prints out thr fisrt, third and fourth characters
+fun characters(name: String) {
     val some = name[0].toString() + name[2] + name[3]
     println(some)
-    credit("Queen", "23 years old")
-    lin()
-    crew()
 }
 
-fun credit(name: String, age: String) {
-    val greet = "Hi my name is"
-    val sum = "I am"
-    val tog = greet + " " + name + " " + sum + " " + age
-    println(tog)
+//function that takes in two parameters and returns a sting with a structure
+fun credits(name: String, age: Int) : String {
+    var greeting = "Hi my name is $name, I am ${age.toString()} years old."
+    return greeting
 }
 
-fun lin() {
-    val lon = "mesopotamia"
-    println(lon.length)
+//function that prints out the length of a string
+fun words(word:String): Int {
+    var size = word.length
+    return size
+
 }
 
-fun crew() {
-    var nme = "Queen"
 
-    if ("Queen" in nme) {
-        println("hey that's me")
+//function that takes in a name and it prints out
+fun crew(name: String) {
+
+    if (name.equals("Queenzy")) {
+        println("Hey that's me")
     }else {
         println("I don't know who that is")
     }
